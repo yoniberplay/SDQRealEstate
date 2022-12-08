5cb4a456019149d72dcaf05131050fa89dea9823
yencarnacion07 <yoniber.encarnacion@gmail.com>
2022-12-07 23:24:34 -0400

    API configured

---
+++ SDQRealEstate.WebApi/Controllers/v1/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDQRealEstate.Presentation.WebApi.Controllers;
+
+namespace SDQRealEstate.WebApi.Controllers.v1
+{
+
+    [ApiVersion("1.0")]
+    public class AccountController : BaseApiController
+    {
+        
+    }
+}
